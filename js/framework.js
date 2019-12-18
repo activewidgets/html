d3b2ea7943fc1ed8eaed5a1d096c047c43d660ce
alex <alex@activewidgets.com>
2019-12-18 10:20:48 +0100

    split js/index into framework, components

---
+++ js/framework.js
@@ -0,0 +1,6 @@
+
+import adapter from '@activewidgets/frameworks/html';
+import * as preact from 'preact';
+
+export const {build, tags, h, DOMTemplate} = adapter(preact);
+
--- js/index.js
+++ js/index.js
@@ -1,9 +1,3 @@
 
-import adapter from '@activewidgets/frameworks/html';
-import * as sources from '@activewidgets/datagrid/js';
-import * as metadata from '@activewidgets/datagrid/metadata';
-import * as preact from 'preact';
-
-export const {build, tags, h, DOMTemplate} = adapter(preact);
-export const {Datagrid, Row, Gridview, Scrollbars} = build(sources, metadata);
-
+export * from './framework.js';
+export * from './datagrid.js';
