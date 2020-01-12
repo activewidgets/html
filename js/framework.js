4e9e61753e53618cbb698fd29c4b0003a9c3ec27
alex <alex@activewidgets.com>
2020-01-12 19:48:30 +0100

    remove DOMTemplate, rename template function

--- js/framework.js
+++ js/framework.js
@@ -3,5 +3,5 @@ import adapter from '@activewidgets/frameworks/html';
 import * as preact from 'preact';
 import htm from 'htm';
 
-export const {h, build, DOMTemplate} = adapter(preact);
-export const html = htm.bind(h);
+export const {h, build} = adapter(preact);
+export const tpl = htm.bind(h);
