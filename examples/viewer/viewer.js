252452b7377239a206127e55922a01b13bd2057f
alex <alex@activewidgets.com>
2020-01-20 23:51:39 +0100

    use shared examples viewer

---
+++ examples/viewer/viewer.js
@@ -0,0 +1,30 @@
+﻿
+import {Viewer} from '@activewidgets/examples';
+import * as pages from '../index.js';
+import '../../';
+
+
+function mount(component, props = {}){
+
+    let container = document.getElementById('app'),
+        target = document.createElement(component);
+
+    Object.keys(props).forEach(i => {
+        target[i] = props[i];
+    });
+
+    container.innerHTML = '';
+    container.appendChild(target);
+}
+
+
+function clean(){
+    let container = document.getElementById('app');
+    container.innerHTML = '';
+}
+
+
+const viewer = new Viewer({
+    target: document.body,
+    props: {pages, mount, clean}
+});
