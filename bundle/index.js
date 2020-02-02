a443b87d301bfde59a5899abc225711484037108
alex <alex@activewidgets.com>
2020-02-02 16:07:02 +0100

    add license header

--- bundle/index.js
+++ bundle/index.js
@@ -4,3 +4,8 @@ import styleInject from 'style-inject';
 export * from '../js/index.js';
 
 styleInject(css, {insertAt: 'top'});
+/**
+ * Copyright (c) ActiveWidgets SARL. All Rights Reserved.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
